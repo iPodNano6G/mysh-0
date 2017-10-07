2c4f0b0a2bcba47c0c96d69ee461170ef5c22f67
iPodNano6G <henry914@ajou.ac.kr>
2017-10-08 05:01:04 +0900

    sh**. no answer.

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,41 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  else
+    chdir(argv[1]);
+
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
+  char CD[256] = {0,};
+  getcwd(CD, 256);
+
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
+  else
+    printf("%s", CD);
   // TODO: Fill it!
 
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if ((strcmp(argv[1], "")) != 0)
+    return 1;
+  else
+    return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if ((strcmp(argv[1], "")) == 0)
+    return 1;
+  else
+    return 0;
 }
