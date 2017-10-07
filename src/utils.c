5d33acc7cfb04d8fee74bdc9cf182e56a1f8bb09
iPodNano6G <henry914@ajou.ac.kr>
2017-10-07 19:10:13 +0900

    modify utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,18 @@
+#include <string.h>
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	char* temp = strtok((char*)command, " ");
+	int i = 0;
+	char* ARGV[] = {NULL};
+
+	while (temp != NULL) {
+		ARGV[i] = temp;
+		argv[i] = &ARGV[i];
+		i = i + 1;
+		temp = strtok(NULL, " ");
+	}
+	argc = &i;
 }
