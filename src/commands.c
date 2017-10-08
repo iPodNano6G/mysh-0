aa040414cb7b68eee9334a12c90504a86aabb7b6
iPodNano6G <henry914@ajou.ac.kr>
2017-10-08 14:26:12 +0900

    fin

--- src/commands.c
+++ src/commands.c
@@ -4,30 +4,30 @@
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
+  int tf = 0;
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  else
-    chdir(argv[1]);
-
-  return 0;
+  else {
+	tf = chdir((char*)argv[1]);
+	if(tf == -1) return -1;
+	if(tf ==  0) return  0;
+  }
 }
 
 int do_pwd(int argc, char** argv) {
-  char CD[256] = {0,};
-  getcwd(CD, 256);
-
   if (!validate_pwd_argv(argc, argv))
     return -1;
-  else
-    printf("%s", CD);
-  // TODO: Fill it!
-
-  return 0;
+  else{
+	char CD[256] = {0,};
+	getcwd(CD, 256);
+	printf("%s\n", CD);
+	return 0;
+  }
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  if ((strcmp(argv[1], "")) != 0)
+  if ((strcmp(argv[1], "")) != 0 && argc == 2)
     return 1;
   else
     return 0;
--- src/utils.c
+++ src/utils.c
@@ -1,18 +1,25 @@
 #include <string.h>
+#include <stdlib.h>
 #include "utils.h"
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-	char* temp = strtok((char*)command, " ");
+	char* temp = strtok((char*)command, " \n");
 	int i = 0;
-	char* ARGV[] = {NULL};
+	
+	(*argv) = (char **) malloc(sizeof(char*)*32);
+
+	for(int i =0; i < 32; i++) {
+		(*argv)[i] = (char*)malloc(sizeof(char)*128);
+		strcpy((*argv)[i], "");
+	}
 
 	while (temp != NULL) {
-		ARGV[i] = temp;
-		argv[i] = &ARGV[i];
+		strcpy((*argv)[i], temp);
 		i = i + 1;
-		temp = strtok(NULL, " ");
+		temp = strtok(NULL, " \n");
 	}
-	argc = &i;
+
+	*argc = i;
 }
